36873de98c21fba80cccdafcaed78d5e6a0b60c2
VincybwoyJR <156839329+VincybwoyJR@users.noreply.github.com>
2024-04-01 16:25:59 -0400

    Finished partTwo
    Forgot to update it but I finished parttwo by adding more constants, attributes, alt names etc.

--- AssignmentFour/PartTwo/gallery-start/main.js
+++ AssignmentFour/PartTwo/gallery-start/main.js
@@ -4,15 +4,34 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : 'Closeup of a persons eye',
+  'pic2.jpg' : 'Wavey rocl',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Egyptian Wall',
+  'pic5.jpg' : 'Moth on leaf'
+}
 
-/* Declaring the alternative text for each image file */
-
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+      });
+    }
+btn.addEventListener('click', () => {
+        const btnClass = btn.getAttribute('class');
+        if (btnClass === 'dark') {
+          btn.setAttribute('class','light');
+          btn.textContent = 'Lighten';
+          overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        } else {
+          btn.setAttribute('class','dark');
+          btn.textContent = 'Darken';
+          overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+        }
+      });
